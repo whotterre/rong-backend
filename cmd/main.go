7fa19b0e8dd807e3c84b5844400fcd63404407d5
whotterre <iJeddy5.0@gmail.com>
2025-07-05 01:13:55 +0100

    refactor Redis client initialization to include retry mechanism and improve error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"github.com/whotterre/tiermaster/internal/config"
+	"github.com/whotterre/tiermaster/internal/conn"
+	"go.uber.org/zap"
+)
+
 func main(){
-	
+	// Initialize logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	// Load config 
+	config, err := config.LoadConfig()
+	if err != nil {
+		logger.Error("Failed to load config from .env file")
+		panic(err)
+	}
+	// Initialize Redis client 
+	client, err := conn.GetRedisClient(config)
+	if err != nil {
+		logger.Error("Failed to initialize Redis client")
+	}
+	// Pass client down to subsequent services
+	println(client)
+
 }
--- internal/conn/conn.go
+++ internal/conn/conn.go
@@ -1,17 +1,56 @@
 package conn
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"github.com/redis/go-redis/v9"
 	"github.com/whotterre/tiermaster/internal/config"
 )
 
-// Initialize Redis client
+// GetRedisClient initializes and returns a Redis client with a retry mechanism.
 func GetRedisClient(cfg config.Config) (*redis.Client, error) {
-	client := redis.NewClient(&redis.Options{
-		Addr:     cfg.RedisAddr,
-		Password: "",
-		DB:       0,
-	})
+	var client *redis.Client
+
+	connectEffector := func(ctx context.Context) (string, error) {
+		c := redis.NewClient(&redis.Options{
+			Addr:     cfg.RedisAddr,
+			Password: "",
+			DB:       0,
+		})
+
+		client = c
+		return "Redis connected", nil
+	}
+
+	// Retry connecting to Redis 5 times with a 2-second delay between attempts
+	retryConnect := Retry(connectEffector, 5, 2*time.Second)
+
+	_, err := retryConnect(context.Background())
+	if err != nil {
+		return nil, err
+	}
 
 	return client, nil
+}
+
+type Effector func(context.Context) (string, error)
+
+func Retry(effector Effector, retries int, delay time.Duration) Effector {
+	return func(ctx context.Context) (string, error) {
+		for r := 0; ; r++ {
+			response, err := effector(ctx)
+			if err == nil || r >= retries {
+				return response, err
+			}
+
+			log.Printf("Attempt %d failed, retrying in %v", r+1, delay)
+			select {
+			case <-time.After(delay):
+			case <-ctx.Done():
+				return "", ctx.Err()
+			}
+		}
+	}
 }
